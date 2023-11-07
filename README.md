c650c05689a2e77f60c32896cf9322843c2a508a
Bela Vogler <bela.vogler@uni-jena.de>
2023-11-07 17:42:01 +0100

    setup push

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # utterly_useful_utilities
  A small collection of general utility functions I hack together and use. Feel free to reach out to me, should you have a request.
+
+
+## Installation
+1. Clone the repository
+2. Run `pip install .` in the root directory of the repository
+3. ...
+4. ...
+5. Profit
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     long_description_content_type='text/markdown',
     license='BSD License',
     readme = readme,
-    packages=find_packages(include=['blobBgone']),
+    packages=find_packages(include=['UUW']),
     author='Bela Tristan Leander Vogler',
     author_email='bela.vogler@uni-jena.de',
     keywords=['Utility', 'Functions', 'Miscellaneous', 'Windowing', 'QR Codes'],
